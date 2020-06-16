2004ba4c83ede61757b10364512c7ffe9a9cca98
Sameer Choudhary <sameerme@iitk.ac.in>
2020-06-16 17:19:31 +0530

    setting up development environment

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class button_layout(GridLayout):
-    def __init__(self,*args,**kwargs):
-        super(button_layout,self).__init__(*args, **kwargs)
-        self.cols = 1
-        self.add_widget(Button(text='Hello'))
-        self.add_widget(Button(text='World')) 
 
 class MyApp(App):
     def build(self):
-        return button_layout()
+	root = Widget()
+        return root
 
 if __name__ == '__main__':
     MyApp().run()
